f07c83a68a359e8fb6be3553ac850b155fae376a
Erin <ekshawhan@gmail.com>
2025-05-11 14:58:12 -0700

    set up project

---
+++ StudyPlanner/Models/StudyTask.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyPlanner.Models
+{
+    class StudyTask
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Subject { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ StudyPlanner/ViewModels/MainViewModel.cs
@@ -0,0 +1,61 @@
+﻿using GalaSoft.MvvmLight.Command;
+using StudyPlanner.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.DirectoryServices;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace StudyPlanner.ViewModels
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<StudyTask> Tasks { get; set; } = new();
+
+        private StudyTask _selectedTask;
+        public StudyTask SelectedTask
+        {
+            get => _selectedTask;
+            set { _selectedTask = value; OnPropertyChanged(); }
+        }
+
+        public ICommand AddTaskCommand { get; }
+        public ICommand DeleteTaskCommand { get; }
+
+        public MainViewModel()
+        {
+            AddTaskCommand = new RelayCommand<ICommand>(_ => AddTask());
+            DeleteTaskCommand = new RelayCommand<ICommand>(_ => DeleteTask(), _ => SelectedTask != null);
+        }
+
+        private void AddTask()
+        {
+            Tasks.Add(new StudyTask
+            {
+                Title = "New Task",
+                Subject = "General",
+                DueDate = DateTime.Today.AddDays(1)
+            });
+        }
+
+        private void DeleteTask()
+        {
+            if (SelectedTask != null)
+            {
+                if (SelectedTask != null)
+                {
+                    Tasks.Remove(SelectedTask);
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string name = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
